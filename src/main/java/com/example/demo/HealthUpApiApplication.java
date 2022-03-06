5e137a33af2e91fca62745dfad27a45cc69506fd
Laura Moreno <73167874+Laumorvel@users.noreply.github.com>
2022-03-06 14:37:09 +0100

    Update HealthUpApiApplication.java

--- src/main/java/com/example/demo/HealthUpApiApplication.java
+++ src/main/java/com/example/demo/HealthUpApiApplication.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
